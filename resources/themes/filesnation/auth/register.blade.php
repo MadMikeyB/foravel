c3c5350850fd9e92f358a88ffbb4b1567d5ffcf1
Michael <mikeybsaccount@gmail.com>
2016-04-17 20:43:54 +0100

    register page

--- resources/themes/filesnation/auth/register.blade.php
+++ resources/themes/filesnation/auth/register.blade.php
@@ -1,39 +1,90 @@
-@extends('layouts.app')
+@extends('layouts.app_alt')
 
 @section('content')
-<div class="box">
-    <div class="row">
-        <div class="12u">
-            <h2>Register</h2>
 
-            <form action="{{url('/register')}}" method="post" role="form">
-                {!! csrf_field() !!}
+    <div class="signup-panel">
+        <div class="left">
+            <h2><span>Sign Up</span></h2>
+            <div class="content-padding">
+                <div class="login-passes">
+                    <b>Log In With:</b>
+                    <a href="/social/facebook" class="strike-tooltip" title="Use Facebook"><i class="fa fa-facebook"></i></a>
+                    <a href="/social/twitter" class="strike-tooltip" title="Use Twitter"><i class="fa fa-twitter"></i></a>
+                    <a href="/social/steam" class="strike-tooltip" title="Use Steam"><i class="fa fa-steam"></i></a>
+                    <a href="/social/google" class="strike-tooltip" title="Use Google.com"><i class="fa fa-google"></i></a>
+                </div>
+                <div class="the-form" style="margin-top:40px;">
+                    <form action="{{url('/register')}}" method="post" role="form">
+                        {!! csrf_field() !!}
 
-                <div class="row uniform 50%">
-                    <div class="12u">
-                        <input id="name" name="name" placeholder="Username" type="text" value="{{ old('name') }}">
-                    </div>
+                        <p>
+                            <label for="name">Username:</label>
+                            <input id="name" name="name" placeholder="Username" type="text" value="{{ old('name') }}">
+                        </p>
 
-                    <div class="12u">
-                        <input id="email" name="email" placeholder="Email Address" type="email" value="{{ old('email') }}">
-                    </div>
+                        <p>
+                            <label for="email">Email Address:</label>
+                            <input id="email" name="email" placeholder="Email Address" type="text" value="{{ old('email') }}">
+                        </p>
 
+                        <p>
+                            <label for="password">Password:</label>
+                            <input id="password" name="password" placeholder="Password" type="password" value="">
+                        </p>
 
-                    <div class="6u 12u(mobilep)">
-                        <input id="password" name="password" placeholder="Password" type="password" value="">
-                    </div>
+                        <p>
+                            <label for="password_confirmation">Confirm Password:</label>
+                            <input id="password_confirmation" name="password_confirmation" placeholder="Confirm Password" type="password" value="">
+                        </p>
+                        
+                        <p class="form-footer">
+                            <input type="submit" name="submit" id="submit" value="Sign Up" />
+                        </p>
 
-                    <div class="6u 12u(mobilep)">
-                        <input id="password_confirmation" name="password_confirmation" placeholder="Confirm Password" type="password" value="">
-                    </div>
+                        <p style="margin-top:40px;">
+                            <span class="info-msg">
+                                <a href="/login">Already got an account?</a><br />
+                            </span>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <h2><span>About {{ Setting::get('site_title') }}</span></h2>
+                <div class="content-padding">
+                    <div class="form-split-about">
+                        <h2>The new home of the greatest gaming-mod community in the world!</h2>
+                        <p class="p-padding">Founded by ex-volunteers of FileFront / GameFront, FilesNation is be the Number 1 place to discuss all things gaming-mods.</p>
+                        <p class="p-padding">
+                        With active, vibrant, up-to-date forums, short-form reporting on the world of modding, and regular podcasts and videos.</p>
+                        <h2>Why Join?</h2>
+                        <ul>
+                            <li>
+                                <i class="fa fa-user"></i>
+                                <b>Your own personalized profile</b>
+                                <p class="p-padding">Your profile will be your FilesNation hub. From a place to display your awards, to a place to post status updates.</p>
+                            </li>
 
-
-                    <div class="12u 12u(mobilep)">
-                        <input class="fit" type="submit" value="Log In">
+                            <li>
+                                <i class="fa fa-trophy"></i>
+                                <b>Contribution Awards</b>
+                                <p class="p-padding">Awards which actually mean something!</p>
+                            </li>
+                            <li>
+                                <i class="fa fa-microphone"></i>
+                                <b>You have a Voice</b>
+                                <p class="p-padding">On FilesNation, anyone can voice thier opinion. Whether you choose add news and reviews about your favourite new games or mods, or post in our Community Forums</p>
+                            </li>
+                            <li>
+                                <i class="fa fa-comments"></i>
+                                <b>Thriving Community</b>
+                                <p class="p-padding">From our custom made forum, to our thought provoking articles and reviews, to our thriving modifications database, there's something for everyone.</p>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </form>
+            </div>
+            <div class="clear-float"></div>
         </div>
-    </div>
-</div>
 @endsection
