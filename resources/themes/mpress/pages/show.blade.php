c311104117b72e6caab165d21d7111bfe9a596a6
Michael <mikeybsaccount@gmail.com>
2016-12-30 05:04:43 +0000

    page template for mpress theme

--- resources/themes/mpress/pages/show.blade.php
+++ resources/themes/mpress/pages/show.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="box">
-	<div class="col-sm-12">
-		<header>
-			<h2>{{ $page->title }}</h2>
-		</header>
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h2 class="panel-title">{{ $page->title }}</h2>
+	</div>
+	<div class="panel-body">
 		{!! $page->content !!}
-		
+	</div>
+	<div class="panel-footer">
 		<ul class="pull-right ">
 			@can( 'edit-page', $page)
 				<li><a href="/{{ $page->slug }}/edit" class="btn btn-primary">Edit</a></li>
